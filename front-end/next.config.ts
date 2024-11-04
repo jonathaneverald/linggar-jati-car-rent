a51b89c54cd018920015773a0806f29e70d85c9e
jonathaneverald <jonathaneverald@gmail.com>
2024-11-05 02:45:04 +0700

    feat: add new packages

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+    /* config options here */
+    reactStrictMode: true,
+    images: {
+        domains: ["pickbazar-react-rest.vercel.app", "images.unsplash.com", "themesflat.co", "res.cloudinary.com", "via.placeholder.com"],
+    },
 };
 
 export default nextConfig;
